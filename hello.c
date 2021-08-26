d221702770a36cba1bc9a05b959eb3afad5ff8e0
Paige Anthony <panthony14@huskers.unl.edu>
2021-08-26 18:03:03 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Paige Anthony
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Paige Anthony\n");
+
+  return 0;
+}
